e526d568b1a516fae56636974d371c0f319f5c44
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-02-06 09:24:55 +0900

    chore: test bun test failure on github actions

--- tests/bun.test.ts
+++ tests/bun.test.ts
@@ -1,5 +1,39 @@
-import { expect, test } from "bun:test";
+import { expect, it, describe, afterAll } from "bun:test";
 
-test("2 + 2", () => {
+it("2 + 2", () => {
   expect(2 + 2).toBe(4);
 });
+
+describe('error', () => {
+  afterAll(() => {
+    boomSuite()
+  })
+
+  it('stack', () => {
+    boom()
+  })
+
+  it('diff', () => {
+    expect({ hello: 'x' }).toEqual({ hello: 'y' })
+  })
+
+  it('unhandled', () => {
+    (async () => boomUnhandled())()
+  })
+})
+
+function boom() {
+  boomInner1()
+}
+
+function boomInner1() {
+  throw new Error('boom')
+}
+
+function boomSuite() {
+  throw new Error('boomSuite')
+}
+
+function boomUnhandled() {
+  throw new Error('boomUnhandled')
+}
